bfc7984833bf9b24906ea746d3ca3683c871d216
Assassin4848 <44311430+Assassin4848@users.noreply.github.com>
2018-12-01 21:34:04 -0800

    Add files via upload

---
+++ Podfile
@@ -0,0 +1,28 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Side Menu' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Side Menu
+
+pod 'FacebookCore'
+pod 'FacebookShare'
+pod 'FacebookLogin'
+pod 'GoogleMaps'
+pod 'GooglePlaces'
+pod 'GooglePlacePicker'
+pod 'ARCarMovement'
+
+  target 'Side MenuTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Side MenuUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ Side Menu/ContainerVC.swift
@@ -0,0 +1,242 @@
+//
+//  ViewController.swift
+//  Side Menu
+//
+//  Created by Sibo Cai on 10/19/18.
+//  Copyright © 2018 Sibo Cai. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+import FacebookLogin
+import GoogleMaps
+import MapKit
+
+protocol HandleMapSearch: class {
+    func dropPinZoomIn(_ placemark:MKPlacemark)
+}
+
+
+class ViewController: UIViewController {
+    
+    var selectedPin: MKPlacemark?
+    var resultSearchController: UISearchController!
+    
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBAction func button3(_ sender: AnyObject) {
+        getDirections()
+    }
+    
+    @IBAction func fblogin(_ sender: UIButton) {
+        //@IBOutlet weak var FBLoginView: UIView!
+    }
+    var dict : [String : AnyObject]!
+    
+  
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager.delegate = self as! CLLocationManagerDelegate
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+        let locationSearchTable = storyboard!.instantiateViewController(withIdentifier: "LocationSearchTable") as! LocationSearchTable
+        resultSearchController = UISearchController(searchResultsController: locationSearchTable)
+        resultSearchController.searchResultsUpdater = locationSearchTable
+        let searchBar = resultSearchController!.searchBar
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Search for places"
+        navigationItem.titleView = resultSearchController?.searchBar
+        resultSearchController.hidesNavigationBarDuringPresentation = false
+        resultSearchController.dimsBackgroundDuringPresentation = true
+        definesPresentationContext = true
+        locationSearchTable.mapView = mapView
+        locationSearchTable.handleMapSearchDelegate = self
+        //if the user is already logged in
+        if FBSDKAccessToken.current() != nil{
+            getFBUserData()
+        }
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(showProfile),
+                                               name: NSNotification.Name("ShowProfile"),
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(showSettings),
+                                               name: NSNotification.Name("ShowSettings"),
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(showSignIn),
+                                               name: NSNotification.Name("ShowSignIn"),
+                                               object: nil)
+        
+    }
+    
+    @objc func showProfile() {
+        performSegue(withIdentifier: "ShowProfile", sender: nil)
+    }
+    
+    @objc func showSettings() {
+        performSegue(withIdentifier: "ShowSettings", sender: nil)
+    }
+    
+    @objc func showSignIn() {
+        performSegue(withIdentifier: "ShowSignIn", sender: nil)
+    }
+    
+    @IBAction func onMoreTapped() {
+        print("TOGGLE SIDE MENU")
+        NotificationCenter.default.post(name: NSNotification.Name("ToggleSideMenu"), object: nil)
+    }
+    
+    override func loadView() {
+        // Create a GMSCameraPosition that tells the map to display the
+        // coordinate -33.86,151.20 at zoom level 6.
+        let camera = GMSCameraPosition.camera(withLatitude: 33.968800, longitude: -117.844460, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: 33.968800, longitude: -117.844460)
+        marker.title = "Diamond Bar"
+        marker.snippet = "United States"
+        marker.map = mapView
+    }
+    @objc func loginButtonClicked() {
+        let loginManager = LoginManager()
+        loginManager.logIn(readPermissions: [.publicProfile], viewController: nil) { loginResult in
+            switch loginResult {
+            case .failed(let error):
+                print(error)
+            case .cancelled:
+                print("User cancelled login.")
+            case .success( _, _, _):
+                print("Logged in!")
+            }
+        }
+    }
+    
+    //function is fetching the user data
+    @objc func getFBUserData(){
+        if((FBSDKAccessToken.current()) != nil){
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, picture.type(large), email"]).start(completionHandler: { (connection, result, error) -> Void in
+                if (error == nil){
+                    self.dict = result as? [String : AnyObject]
+                    print(result!)
+                    print(self.dict)
+                }
+            })
+        }
+    }
+    
+    @objc func getDirections(){
+        guard let selectedPin = selectedPin else { return }
+        let mapItem = MKMapItem(placemark: selectedPin)
+        let launchOptions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
+        mapItem.openInMaps(launchOptions: launchOptions)
+    }
+    }
+    
+    //when login button clicked
+
+extension ViewController : CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.requestLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else { return }
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: location.coordinate, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("error:: \(error)")
+    }
+    
+}
+
+extension ViewController: HandleMapSearch {
+    
+    func dropPinZoomIn(_ placemark: MKPlacemark){
+        // cache the pin
+        selectedPin = placemark
+        // clear existing pins
+        mapView.removeAnnotations(mapView.annotations)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = placemark.coordinate
+        annotation.title = placemark.name
+        
+        if let city = placemark.locality,
+            let state = placemark.administrativeArea {
+            annotation.subtitle = "\(city) \(state)"
+        }
+        
+        mapView.addAnnotation(annotation)
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: placemark.coordinate, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+    
+}
+
+extension ViewController : MKMapViewDelegate {
+    
+    private func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView?{
+        
+        guard !(annotation is MKUserLocation) else { return nil }
+        
+        let reuseId = "pin"
+        guard let pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView else { return nil }
+        
+        pinView.pinTintColor = UIColor.orange
+        pinView.canShowCallout = true
+        let smallSquare = CGSize(width: 30, height: 30)
+        var button: UIButton?
+        button = UIButton(frame: CGRect(origin: CGPoint.zero, size: smallSquare))
+        button?.setBackgroundImage(UIImage(named: "car"), for: UIControl.State())
+        button?.addTarget(self, action: #selector(ViewController.getDirections), for: .touchUpInside)
+        pinView.leftCalloutAccessoryView = button
+        
+        return pinView
+    }
+}
+
+
+class ContainerVC: UIViewController {
+
+    @IBOutlet weak var sideMenuConstraint: NSLayoutConstraint!
+    var sideMenuOpen = false
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(toggleSideMenu),
+                                               name: NSNotification.Name("ToggleSideMenu"),
+                                               object: nil)
+    }
+    
+    @objc func toggleSideMenu() {
+        if sideMenuOpen {
+            sideMenuOpen = false
+            sideMenuConstraint.constant = -240
+            
+        } else {
+            sideMenuOpen = true
+            sideMenuConstraint.constant = 0
+        }
+        UIView.animate(withDuration: 0.3){
+            self.view.layoutIfNeeded()
+    }
+    }
+    
+}
+
